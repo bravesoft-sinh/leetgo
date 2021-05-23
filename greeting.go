c4f9d34a00abec0c9feb3a9bd3b3f2defb61c091
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2021-05-23 23:24:44 +0900

    add selection sort

--- greeting.go
+++ greeting.go
@@ -1,13 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("1 + 2 = ", add(1, 2))
+	go say("world")
+	say("hello")
 }
 
 func add(a int, b int) int {
 	return a + b
+}
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 
--- sort.go
+++ sort.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var arr = [5]int{2, 3, 1, 5, 4}
+	var arr = []int{2, 3, 1, 5, 4}
+	fmt.Println()
+	fmt.Println("Bubble Sort")
 	arr = bubbleSort(arr)
 	printArray(arr)
-	arr = sort([5]int{2, 3, 1, 5, 4})
+	fmt.Println()
+	fmt.Println("Big O Sort")
+	arr = sort([]int{2, 3, 1, 5, 4})
 	printArray(arr)
+	fmt.Println()
+	fmt.Println("Selection Sort")
+	printArray(selectionSort([]int{3, 2, 4, 5, 1}))
 }
-func sort(arr [5]int) [5]int {
+func sort(arr []int) []int {
 	var n int = len(arr)
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
@@ -22,7 +29,7 @@ func sort(arr [5]int) [5]int {
 	}
 	return arr
 }
-func bubbleSort(arr [5]int) [5]int {
+func bubbleSort(arr []int) []int {
 	var n = len(arr)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n-i-1; j++ {
@@ -35,9 +42,30 @@ func bubbleSort(arr [5]int) [5]int {
 	}
 	return arr
 }
-func printArray(arr [5]int) {
+
+func selectionSort(arr []int) []int {
+	var n = len(arr)
+	for i := 0; i < n; i++ {
+		var indexMin = i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[indexMin] {
+				indexMin = j
+			}
+		}
+		swap(arr, i, indexMin)
+	}
+	return arr
+}
+
+func printArray(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		fmt.Print(arr[i])
 		fmt.Print(" ")
 	}
+}
+
+func swap(arr []int, i int, j int) {
+	var temp = arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
 }
